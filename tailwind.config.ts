8b80aae2088a2ad32f5bdc2f4538499abf73b564
ByteAtATime <byteatatime@proton.me>
2025-07-10 09:35:20 -0700

    feat: update font to Space Grotesk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
 				sm: 'calc(var(--radius) - 4px)'
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				sans: ['"Space Grotesk Variable"', ...fontFamily.sans]
 			},
 			keyframes: {
 				'accordion-down': {
